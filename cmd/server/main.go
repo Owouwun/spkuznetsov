9097e367560e11a4e3402afd3bcc49b591f5049c
seagumineko <kuznetsovivangio@gmail.com>
2025-09-12 20:57:06 +0300

    feat: Add server launching

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,33 @@
 package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+
+	"github.com/Owouwun/spkuznetsov/internal/core/api/handlers"
+	"github.com/Owouwun/spkuznetsov/internal/core/logic/orders"
+)
+
+type orderServiceImpl struct{}
+
+func (s *orderServiceImpl) CreateNewOrder(pord *orders.PrimaryOrder) (*orders.Order, error) {
+	return pord.CreateNewOrder()
+}
+
+func main() {
+	router := gin.Default()
+
+	// DI
+	orderService := &orderServiceImpl{}
+	orderHandler := handlers.NewOrderHandler(orderService)
+
+	// Routing
+	router.POST("/orders", orderHandler.CreateNewOrder)
+
+	// Launch
+	if err := router.Run(":8080"); err != nil && err != http.ErrServerClosed {
+		log.Fatalf("failed to start server: %v", err)
+	}
+}
